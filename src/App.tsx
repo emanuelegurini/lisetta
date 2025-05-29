c0d2472de25e6f43671569e7b45fddce2941cba5
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-05-29 17:07:26 +0200

    Add Supabase client and fetch properties in App component; update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react';
+import { createClient } from "@supabase/supabase-js";
+
+
+// TODO: creare un layer a parte
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL 
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+const supabase =  createClient(supabaseUrl, supabaseKey)
+
+const get_properties = async () => {
+      const { data, error } = await supabase
+        .from('realty')
+        .select('id, region (id, region_name), city (id, city_name), url')
+      if (error) throw error;
+
+      return data;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  // TODO: eliminare gli any
+  const [properties, setProperties] = useState<any>([])
 
+     useEffect(() => {
+      const fetchProperties = async () => {
+        try {
+          const data = await get_properties(); 
+          setProperties([...data])
+        } catch (err) {
+          console.error("Error:", err);
+        } finally {
+          console.log('finish')
+        }
+      };
+
+      fetchProperties();
+     }, [])
+
+     useEffect(() => {
+      console.log(properties)
+     }, [properties])
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p>Realty</p>
+      <ul>
+        {
+          properties.map(property => {
+            return (
+            <li>
+              <p>{property.id}</p>
+              <p>{property.city.city_name}</p>
+              <a href={property.url} target='__blank'>Link</a>
+            </li>
+          )
+          })
+        }
+      </ul>
+    </div>
   )
 }
 
