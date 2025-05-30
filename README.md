9692e4486d0f8b391e85cb8135b190ca519e09b4
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-05-30 13:09:18 +0200

    Refactored property display to utilize new components and improve user experience.

--- README.md
+++ README.md
@@ -5,3 +5,34 @@ Un'idea per tenere in un unico posto gli immobili che si stanno tenendo sotto co
 Da affittare? Da comprare? BOH!
 Fai tu!
 
+## Workflow degli appuntamenti
+
+1. new → appena aggiunto
+2. viewed → visto online
+3. appointment_requested → richiesta inviata
+4. appointment_confirmed → confermato con data
+5. visited → visitato fisicamente
+6. interested/rejected → decisione finale
+
+App => property_portal_id
+Via => address [x]
+Visitato => viewing_status [x]
+Agenzia => real_estate_agencies
+metri_quadrati => surface area [x]
+Euro al metro quadro => è sul portale [x]
+Costo => price [x]
+Piano => floor [x]
+Ascensore => elevator [x]
+APE => energy_classes
+Balcone => balcony [x]
+Giardino => garden [x]
+Spese condominio => condo_fees
+Garage => garage [x]
+Posto auto => car_park [x]
+Cantina => basement [x]
+Cantina Ciclabile => cicle basement [x]
+Orientamento
+Riscaldamento => heating_system [x]
+Climatizzazione => air_conditioning [x]
+Da ristrutturare => needs_renovation [x]
+Note [x]
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,31 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 import {
   Table,
@@ -16,6 +41,22 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+export const getViewingStatusIcon = (status) => {
+  const iconMap = {
+    new: "🆕",
+    viewed: "👁️",
+    appointment_requested: "📞",
+    appointment_confirmed: "📅",
+    visited: "👁️",
+    interested: "❤️",
+    rejected: "❌",
+    archived: "📦",
+  };
+
+  return iconMap[status] || "❓";
+};
+
+
 // TODO: creare un layer a parte
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL 
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
@@ -24,24 +65,58 @@ const supabase =  createClient(supabaseUrl, supabaseKey)
 
 const get_properties = async () => {
       const { data, error } = await supabase
-        .from('realty')
-        .select(`
+      .from("realty")
+      .select(`
+        id,
+        address,
+        price,
+        condo_fees,
+        surface_area,
+        floor_number,
+        needs_renovation,
+        basement,
+        cycle_basement,
+        elevator,
+        air_conditioning,
+        balcony,
+        garden,
+        garage,
+        car_park,
+        lat,
+        long,
+        url,
+        note,
+        city (
           id,
-          address,
-          price,
-          surface_area,
-          region (
-            id,
-            region_name
-          ),
-          city (
-            id,
-            city_name
-          ),
-          url,
-          lat,
-          long
-        `)
+          city_name
+        ),
+        region (
+          id,
+          region_name
+        ),
+        heating_systems (
+          id,
+          heat_system
+        ),
+        viewing_statuses (
+          id, 
+          viewing_status
+        ), 
+        energy_classes (
+          id,
+          energy_class
+        ),
+        property_portals (
+          id,
+          property_portals_name
+        ), 
+        real_estate_agencies (
+          id,
+          real_estate_agency,
+          real_estate_agency_url
+        )
+      `);
+
       if (error) throw error;
 
       return data;
@@ -112,11 +187,13 @@ const PropertyPopup = ({ property }) => {
 function App() {
   // TODO: eliminare gli any
   const [properties, setProperties] = useState<any>([])
+  const [showMap, setShowMap] = useState<boolean>(true)
 
      useEffect(() => {
       const fetchProperties = async () => {
         try {
           const data = await get_properties(); 
+          console.log('data:', data)
           setProperties([...data])
         } catch (err) {
           console.error("Error:", err);
@@ -130,123 +207,253 @@ function App() {
      useEffect(() => {
       console.log(properties)
      }, [properties])
+
+     const handleShowMap = () => {
+      setShowMap((prev) => !prev)
+     }
   
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        {/* Mappa */}
-        <Card className="mb-8">
-          <CardContent>
-            <MapContainer
-              center={[44.493936, 11.342744]}
-              zoom={13}
-              scrollWheelZoom={false}
-              className="h-full w-full"
-            >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-
-              {properties.map((property) => (
-                <Marker
-                  key={property.id}
-                  position={[property.lat, property.long]}
+      <main className="m-auto w-full max-w-[1200px] flex-1 p-8">
+        <div className="pb-8 pt-4 flex justify-between items-center mb-8">
+          <NavigationMenu>
+            <NavigationMenuList className="flex space-x-4">
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="/"
+                  className="text-gray-700 hover:font-bold transition-colors duration-300"
                 >
-                  <Popup>
-                    <PropertyPopup
-                      property={{
-                        id: 3,
-                        price: "€280.000",
-                        sqm: 95,
-                        ape: "A",
-                        bikeCellar: true,
-                        balcony: false,
-                        parking: true,
-                        garage: true,
-                        address: "Via Dante 22, Firenze",
-                      }}
-                    />
-                  </Popup>
-                </Marker>
-              ))}
-            </MapContainer>
-          </CardContent>
-        </Card>
+                  Home
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              {/*               <NavigationMenuItem>
+                <NavigationMenuLink
+                  href="/account"
+                  className="text-gray-700 hover:font-bold transition-colors duration-300"
+                >
+                  Account
+                </NavigationMenuLink>
+              </NavigationMenuItem> */}
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="flex items-center space-x-2">
+            <span className="text-gray-700 capitalize">Irene</span>
+            <Avatar>
+              <AvatarImage src={"https://github.com/shadcn.png"} />
+              <AvatarFallback>
+                {"irene giacchetta"
+                  .split(" ")
+                  .slice(0, 2)
+                  .map((name: string) => name[0].toUpperCase())
+                  .join("")}
+              </AvatarFallback>
+            </Avatar>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center space-x-2">
+            <Switch checked={showMap} onCheckedChange={handleShowMap} />
+            <Label htmlFor="airplane-mode">
+              {showMap ? "Nascondi" : "Mostra"} Mappa
+            </Label>
+          </div>
+          {/* Mappa */}
+          {showMap && (
+            <Card className="mb-8">
+              <CardContent>
+                <MapContainer
+                  center={[44.493936, 11.342744]}
+                  zoom={13}
+                  scrollWheelZoom={false}
+                  className="h-full w-full"
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
 
-        <Card>
-          <CardContent>
-            <Table>
-              <TableCaption>Elenco delle tue proprietà preferite.</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[200px]">Indirizzo</TableHead>
-                  <TableHead>Prezzo</TableHead>
-                  <TableHead>Mq</TableHead>
-                  <TableHead>Prezzo/Mq</TableHead>
-                  <TableHead>Piano</TableHead>
-                  <TableHead>Ascensore</TableHead>
-                  <TableHead>APE</TableHead>
-                  <TableHead>App</TableHead>
-                  <TableHead>Agenzia</TableHead>
-                  <TableHead>Garage</TableHead>
-                  <TableHead>Balcone</TableHead>
-                  <TableHead>Spese Cond.</TableHead>
-                  <TableHead>Giardino</TableHead>
-                  <TableHead>Riscaldamento</TableHead>
-                  <TableHead>Climatizzazione</TableHead>
-                  <TableHead>Orientamento</TableHead>
-                  <TableHead>Cantina</TableHead>
-                  <TableHead>Da Ristrutturare</TableHead>
-                  <TableHead>Visitato</TableHead>
-                  <TableHead>Cantina Ciclabile</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {properties.map((property) => (
+                  {properties.map((property) => (
+                    <Marker
+                      key={property.id}
+                      position={[property.lat, property.long]}
+                    >
+                      <Popup>
+                        <PropertyPopup
+                          property={{
+                            id: 3,
+                            price: "€280.000",
+                            sqm: 95,
+                            ape: "A",
+                            bikeCellar: true,
+                            balcony: false,
+                            parking: true,
+                            garage: true,
+                            address: "Via Dante 22, Firenze",
+                          }}
+                        />
+                      </Popup>
+                    </Marker>
+                  ))}
+                </MapContainer>
+              </CardContent>
+            </Card>
+          )}
+
+          <Card>
+            <CardContent>
+              <Table>
+                <TableCaption>
+                  Elenco delle tue proprietà preferite.
+                </TableCaption>
+                <TableHeader>
                   <TableRow>
-                    <TableCell className="font-medium">
-                      <a href={property.url} target="__blank">
-                        {property.address}, {property.city.city_name}
-                      </a>
-                    </TableCell>
-                    <TableCell>
-                      {property.price.toLocaleString("it-IT", {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2,
-                      })}
-                    </TableCell>
-                    <TableCell>{property.surface_area}</TableCell>
-                    <TableCell>
-                      €
-                      {(property.price / property.surface_area).toLocaleString(
-                        "it-IT",
-                        { minimumFractionDigits: 2, maximumFractionDigits: 2 }
-                      )}
-                    </TableCell>
-                    <TableCell>3°</TableCell>
-                    <TableCell>✓</TableCell>
-                    <TableCell>B</TableCell>
-                    <TableCell>Idealista</TableCell>
-                    <TableCell>Tecnocasa</TableCell>
-                    <TableCell>✓</TableCell>
-                    <TableCell>✓</TableCell>
-                    <TableCell>€120/mese</TableCell>
-                    <TableCell>✗</TableCell>
-                    <TableCell>Autonomo</TableCell>
-                    <TableCell>✓</TableCell>
-                    <TableCell>Sud</TableCell>
-                    <TableCell>✓</TableCell>
-                    <TableCell>✗</TableCell>
-                    <TableCell>✓</TableCell>
-                    <TableCell>✓</TableCell>
+                    <TableHead>Status</TableHead>
+                    <TableHead className="w-[200px]">Indirizzo</TableHead>
+                    <TableHead>Prezzo</TableHead>
+                    <TableHead>Mq</TableHead>
+                    <TableHead>Prezzo/Mq</TableHead>
+                    <TableHead>Piano</TableHead>
+                    <TableHead>Ascensore</TableHead>
+                    <TableHead>APE</TableHead>
+                    <TableHead>App</TableHead>
+                    <TableHead>Agenzia</TableHead>
+                    <TableHead>Garage</TableHead>
+                    <TableHead>Balcone</TableHead>
+                    <TableHead>Spese Cond.</TableHead>
+                    <TableHead>Giardino</TableHead>
+                    <TableHead>Riscaldamento</TableHead>
+                    <TableHead>Climatizzazione</TableHead>
+                    {/* <TableHead>Orientamento</TableHead> */}
+                    <TableHead>Cantina</TableHead>
+                    <TableHead>Cantina Ciclabile</TableHead>
+                    <TableHead>Da Ristrutturare</TableHead>
+                    <TableHead>Note</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
-      </div>
+                </TableHeader>
+                <TableBody>
+                  {properties.map((property) => (
+                    <TableRow>
+                      <TableCell>
+                        {getViewingStatusIcon(
+                          property.viewing_statuses.viewing_status
+                        )}
+                      </TableCell>
+                      <TableCell className="font-medium">
+                        <a href={property.url} target="__blank">
+                          {property.address}, {property.city.city_name}
+                        </a>
+                      </TableCell>
+                      <TableCell>
+                        {property.price.toLocaleString("it-IT", {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        })}
+                      </TableCell>
+                      <TableCell>{property.surface_area}</TableCell>
+                      <TableCell>
+                        €
+                        {(
+                          property.price / property.surface_area
+                        ).toLocaleString("it-IT", {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        })}
+                      </TableCell>
+                      <TableCell>{property.floor_number}°</TableCell>
+                      <TableCell>{property.elevator ? "✓" : "✗"}</TableCell>
+                      <TableCell
+                        className={getAPEColor(
+                          property.energy_classes.energy_class
+                        )}
+                      >
+                        {property.energy_classes.energy_class}
+                      </TableCell>
+                      <TableCell>
+                        {property.property_portals.property_portals_name}
+                      </TableCell>
+                      <TableCell>
+                        <a
+                          href={
+                            property.real_estate_agencies.real_estate_agency_url
+                          }
+                          target="__blank"
+                        >
+                          {property.real_estate_agencies.real_estate_agency}
+                        </a>
+                      </TableCell>
+                      <TableCell>{property.garage ? "✓" : "✗"}</TableCell>
+                      <TableCell>{property.balcony ? "✓" : "✗"}</TableCell>
+                      <TableCell>
+                        €
+                        {property.condo_fees.toLocaleString("it-IT", {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        })}
+                        /mese
+                      </TableCell>
+                      <TableCell>{property.garden ? "✓" : "✗"}</TableCell>
+                      <TableCell>
+                        {property.heating_systems.heat_system}
+                      </TableCell>
+                      <TableCell>
+                        {property.air_conditioning ? "✓" : "✗"}
+                      </TableCell>
+                      {/* <TableCell>Sud</TableCell> */}
+                      <TableCell>{property.basement ? "✓" : "✗"}</TableCell>
+                      <TableCell>
+                        {property.cycle_basement ? "✓" : "✗"}
+                      </TableCell>
+                      <TableCell>
+                        {property.needs_renovation ? "✓" : "✗"}
+                      </TableCell>
+                      <TableCell>
+                        {/** Drawer */}
+
+                        <Drawer>
+                          <DrawerTrigger asChild>
+                            <Button variant="outline">Open Drawer</Button>
+                          </DrawerTrigger>
+                          <DrawerContent className="z-[1000]">
+                            <div className="mx-auto w-full max-w-sm">
+                              <DrawerHeader>
+                                <DrawerTitle>Move Goal</DrawerTitle>
+                                <DrawerDescription>
+                                  Set your daily activity goal.
+                                </DrawerDescription>
+                              </DrawerHeader>
+                              <div className="p-4 pb-0">
+                                <div className="flex items-center justify-center space-x-2">
+                                  <div className="flex-1 text-center">
+                                    <div className="text-7xl font-bold tracking-tighter">
+                                      testo
+                                    </div>
+                                    <div className="text-muted-foreground text-[0.70rem] uppercase">
+                                      Calories/day
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="mt-3 h-[120px]">contenuto</div>
+                              </div>
+                              <DrawerFooter>
+                                <Button>Submit</Button>
+                                <DrawerClose asChild>
+                                  <Button variant="outline">Cancel</Button>
+                                </DrawerClose>
+                              </DrawerFooter>
+                            </div>
+                          </DrawerContent>
+                        </Drawer>
+
+                        {/** End - Drawer */}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </div>
+      </main>
     </div>
   );
 }
