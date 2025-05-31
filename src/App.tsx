af84fa02b55ede22bbd73e8006a817e82e6d9065
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-05-31 12:02:06 +0200

    feat: add Supabase authentication UI components and update session management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
+import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { BrowserRouter, Routes } from 'react-router';
 import { createRoutes } from './routes/routes';
+
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 // TODO: creare un layer a parte
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL 
@@ -10,16 +14,31 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 export const supabase =  createClient(supabaseUrl, supabaseKey)
 
 function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
 
   const routesList = createRoutes()
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        {routesList.routes.map(({ component }) => component())}
-      </Routes>
-    </BrowserRouter>
-  );
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return (
+      <BrowserRouter>
+        <Routes>{routesList.routes.map(({ component }) => component())}</Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App
